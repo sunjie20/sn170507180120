5ba2c2d3effe43961a70bd706a96d88b54ddbe7f
sunjie20 <419819090@qq.com>
2020-04-03 12:10:33 +0800

    New changelist

---
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180120/Ch4Activity3.java
@@ -0,0 +1,13 @@
+package cn.edu.sdwu.android.classroom.sn170507180120;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ch4Activity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_ch4_3);
+    }
+}
